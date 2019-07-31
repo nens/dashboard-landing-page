6b3ed75a964d087db236362b6c80952ec7b8b4f5
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-07-31 09:59:28 +0200

    add release config

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -5,4 +5,4 @@ DIRECTORY=`pwd`
 
 mkdir -p $DIRECTORY/dist
 zip -r $DIRECTORY/dist/klimaatatlas-client.zip $DIRECTORY/build/*
-ls -l $DIRECTORY/dist/klimaatatlas-client.zip
+ls -l $DIRECTORY/dist/dashboard-landing-page.zip
